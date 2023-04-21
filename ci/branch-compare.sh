c542ad04c65aacb6383bdb976fd4c1af9451cb47
Sukanya M <64667488+sukanya5960@users.noreply.github.com>
2023-04-21 23:34:57 +0530

    test

--- ci/branch-compare.sh
+++ ci/branch-compare.sh
@@ -3,9 +3,10 @@ set -e
 git config user.name $USERNAME
 git config user.email $EMAIL
 git config pull.rebase false
-git checkout -b $ORIGIN_BRANCH origin/$ORIGIN_BRANCH
+git fetch --all
+git checkout $ORIGIN_BRANCH 
 git pull origin $ORIGIN_BRANCH
-git checkout -b $MERGE_BRANCH origin/$MERGE_BRANCH
+git checkout $MERGE_BRANCH
 git pull origin $MERGE_BRANCH
 git branch
 git merge --no-commit --no-ff $ORIGIN_BRANCH  | tee automerge.out
